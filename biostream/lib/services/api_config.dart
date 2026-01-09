ffca2f1bb9233f1d140d19afcf18c91fc494bbc8
JellyJerry0u0 <wecd.ds02@gmail.com>
2026-01-09 19:40:55 +0900

    web compile error fix

--- biostream/lib/services/api_config.dart
+++ biostream/lib/services/api_config.dart
@@ -1,4 +1,3 @@
-import 'dart:io' if (dart.library.html) 'dart:html';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 import 'package:device_info_plus/device_info_plus.dart';
@@ -27,41 +26,39 @@ class ApiConfig {
       return "http://localhost:8080";
     }
 
-    // 개발 모드: 에뮬레이터/시뮬레이터 여부를 구분
-    if (!kIsWeb && Platform.isAndroid) {
-      final isEmulator = await _isEmulator();
-      return isEmulator ? "http://10.0.2.2:8080" : "http://127.0.0.1:8080";
-    }
-    if (!kIsWeb && Platform.isIOS) {
-      final isSimulator = await _isEmulator();
-      return isSimulator ? "http://127.0.0.1:8080" : "http://127.0.0.1:8080";
+    // 개발 모드: 에뮬레이터/시뮬레이터 여부를 구분 (웹이 아닐 때만)
+    return _getMobileOrigin();
+  }
+
+  // 모바일 플랫폼용 오리진 (웹에서는 호출되지 않음)
+  static Future<String> _getMobileOrigin() async {
+    if (kIsWeb) return "http://localhost:8080"; // 웹에서는 호출되지 않지만 안전장치
+
+    final deviceInfo = DeviceInfoPlugin();
+    try {
+      // Android 체크
+      try {
+        final androidInfo = await deviceInfo.androidInfo;
+        final isEmulator = !androidInfo.isPhysicalDevice;
+        return isEmulator ? "http://10.0.2.2:8080" : "http://127.0.0.1:8080";
+      } catch (_) {
+        // Android가 아님
+      }
+
+      // iOS 체크
+      try {
+        final iosInfo = await deviceInfo.iosInfo;
+        final isSimulator = !iosInfo.isPhysicalDevice;
+        return isSimulator ? "http://127.0.0.1:8080" : "http://127.0.0.1:8080";
+      } catch (_) {
+        // iOS가 아님
+      }
+    } catch (_) {
+      // 정보 획득 실패
     }
 
     // 기타 플랫폼 기본값
     return "http://127.0.0.1:8080";
-  }
-
-  static Future<bool> _isEmulator() async {
-    // 웹에서는 에뮬레이터 체크 불필요
-    if (kIsWeb) return false;
-
-    final deviceInfo = DeviceInfoPlugin();
-    try {
-      if (Platform.isAndroid) {
-        final info = await deviceInfo.androidInfo;
-        // true면 실기기, false면 에뮬레이터
-        return !info.isPhysicalDevice;
-      }
-      if (Platform.isIOS) {
-        final info = await deviceInfo.iosInfo;
-        // true면 실기기, false면 시뮬레이터
-        return !info.isPhysicalDevice;
-      }
-    } catch (_) {
-      // 정보 획득 실패 시 "실기기"로 간주(=자동 변환을 과신하지 않도록)
-      return false;
-    }
-    return false;
   }
 
   // 오리진 저장
